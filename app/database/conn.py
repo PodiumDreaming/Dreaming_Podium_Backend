c7c973c1d4dc6fab3d7d55fc3b895b5524ed1da2
Minhyeok <jeongmh09@khu.ac.kr>
2021-11-13 21:20:04 +0900

    AWS RDS connection.

--- app/database/conn.py
+++ app/database/conn.py
@@ -1,9 +1,11 @@
 from sqlalchemy import  create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 import pymysql
 
-db_url = "mysql+pymysql://root:jeongmh0902@localhost:3306/dptest"
+Remote = os.getenv("MariaDB")
+db_url = f"mysql+pymysql://admin:jeongmh0902@{Remote}:3306/MariaDB"
 
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
